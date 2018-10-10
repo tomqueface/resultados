350c30e19a5798aa673125025c0110644bf840c8
tomqueface <42480091+tomqueface@users.noreply.github.com>
2018-10-10 15:31:41 +0100

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,25 +4,25 @@
     if ($('#dashboard-area-chart').length) {
       var lineChartCanvas = $("#dashboard-area-chart").get(0).getContext("2d");
       var data = {
-        labels: ["2013", "2014", "2014", "2015", "2016", "2017"],
+        labels: ["2008", "2009", "2012", "2013", "2017", "2018"],
         datasets: [{
-            label: 'Product',
-            data: [0, 11, 6, 10, 8, 0],
-            backgroundColor: 'rgba(0, 128, 207, 0.4)',
+            label: 'Frelimo',
+            data: [0, 46, 49, 49, 4, 0],
+            backgroundColor: 'rgba(255, 0, 0, 0.4)',
             borderWidth: 1,
             fill: true
           },
           {
-            label: 'Product',
-            data: [0, 7, 11, 8, 11, 0],
-            backgroundColor: 'rgba(2, 178, 248, 0.4)',
+            label: 'Renamo',
+            data: [0, 0, 0, 0, 1, 0],
+            backgroundColor: 'rgba(O, 255, 0, 0.4)',
             borderWidth: 1,
             fill: true
           },
           {
-            label: 'Support',
-            data: [0, 14, 10, 14, 6, 0],
-            backgroundColor: 'rgba(73, 221, 255, 0.4)',
+            label: 'MDM',
+            data: [0, 1, 1, 3, 2, 0],
+            backgroundColor: 'rgba(0, 0, 255, 0.4)',
             borderWidth: 1,
             fill: true
           }
